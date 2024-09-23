7a8b817d85109dcf95a7f5163741278313c50ea1
Huang Junwei Justin <13817117719@163.com>
2024-09-23 13:11:07 -0700

    SelectedCounterVisual
    111

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,38 +1,151 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public event EventHandler<OnselectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnselectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }
+
+
+
     //serializeField is used to make the variable visible in the inspector
     //in the same time, it is private
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private GameInput gameInput;
-
+    [SerializeField] private LayerMask counterLayerMask;
 
     private bool isWalking;
 
-    private void Update()
+    private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
 
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        if(selectedCounter != null)
+        {
+            selectedCounter.Interact();
+        }
+    }
 
-        //if use =, the object will move to the position of the inputVector
-        //if use +=, the object will move to the position of the inputVector + the current position
-
-        Vector3 moveDir=new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * moveSpeed* Time.deltaTime;
-
-        isWalking = moveDir != Vector3.zero;
-
-        //transform.forward is the direction that the object is facing
-        float rotateSpeed = 10f;    
-        transform.forward =Vector3.Slerp(transform.forward, moveDir,Time.deltaTime*rotateSpeed);
+    private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
     }
 
     public bool IsWalking()
     {
         return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        if(moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+
+        //Raycast is used to detect if there is any object in the way
+        //out RaycastHit raycastHit is used to store the information of the object that is detected
+
+        //2:34:44
+
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance,counterLayerMask))
+        {
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Has ClearCounter component
+                if(clearCounter!=selectedCounter)
+                {
+                    selectedCounter = clearCounter;
+
+                    OnSelectedCounterChanged?.Invoke(this, new OnselectedCounterChangedEventArgs { selectedCounter = selectedCounter });
+                }
+            }
+            else
+            {
+                selectedCounter = null;
+                OnSelectedCounterChanged?.Invoke(this, new OnselectedCounterChangedEventArgs { selectedCounter = selectedCounter });
+            }
+
+        }
+        else
+        {
+            selectedCounter = null;
+            OnSelectedCounterChanged?.Invoke(this, new OnselectedCounterChangedEventArgs { selectedCounter = selectedCounter });
+        }
+
+        Debug.Log(selectedCounter);
+
+    }
+    private void HandleMovement()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        //if use =, the object will move to the position of the inputVector
+        //if use +=, the object will move to the position of the inputVector + the current position
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        //Raycast is used to detect if there is any object in the way
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+
+
+        isWalking = moveDir != Vector3.zero;
+
+        //transform.forward is the direction that the object is facing
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
 }
